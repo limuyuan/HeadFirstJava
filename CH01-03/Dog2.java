4d84ab00ccfed1e07cff9d59206fc893f82c1abb
Morris Li <limuyuan@users.noreply.github.com>
2017-10-25 11:35:17 +0800

    Create Dog2.java

---
+++ CH01-03/Dog2.java
@@ -0,0 +1,22 @@
+class Dog2
+{
+  int size;
+  String breed;
+  String name;
+
+  void bark()
+  {
+    System.out.println("Ruff! Ruff!");
+  }
+}
+
+class DogTestDrive
+{
+  public static void main (String[] args)
+  {
+    //Dog test code goes here
+    Dog d = new Dog();
+    d.size = 40;
+    d.bark();
+  }
+}
